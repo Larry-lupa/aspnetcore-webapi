3550e33894bed5014560a04f5516737e89a8921c
lfarias <lfarias@calrom.com>
2023-06-28 18:40:59 +0100

    Books table added to db and first migration file added.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")));
+    builder.Configuration.GetConnectionString("DefaultConnectionString")));
 //new commecnt to test
 
 var app = builder.Build();
